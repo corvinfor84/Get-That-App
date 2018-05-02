749e60317fd3e4da745d3e5fe0b034c96559cf89
Jason Payne <senorpayne@yahoo.com>
2018-05-01 22:43:28 -0500

    server/scrapper

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 var cheerio = require("cheerio");
 var request = require("request");
 
-// First, tell the console what server.js is doing
-console.log("\n***********************************\n" +
-            "Grabbing every thread name and link\n" +
-            "from reddit's webdev board:" +
-            "\n***********************************\n");
 
-// Making a request for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
 request("http://www.amazon.com/gp/search/keywords=macbook&low-price=1000&high-price=2000", 
   function(error, response, html) {
     //console.log(html);
     //console.log(error)
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
   var $ = cheerio.load(html);
 
   // An empty array to save the data that we'll scrape
   var results = [];
 
   // With cheerio, find each p-tag with the "title" class
-  // (i: iterator. element: the current element)
   $("div.a-fixed-left-grid-col.a-col-right").each(function(i, element) {
     //console.log(element);
     console.log($(element).children().children().children().attr("title"));
@@ -41,6 +32,5 @@ request("http://www.amazon.com/gp/search/keywords=macbook&low-price=1000&high-pr
     */
   });
 
-  // Log the results once you've looped through each of the elements found with cheerio
   console.log(results);
 });
