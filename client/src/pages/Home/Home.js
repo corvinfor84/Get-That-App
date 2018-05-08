d2b69bdf53f4538f20a1d1daa316b33b2d474348
Cornell <corvinforeman@gmail.com>
2018-05-08 15:14:23 -0500

    home page notes

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -10,10 +10,10 @@ class Home extends React.Component {
 	render() {
 		return (
 				<div>
-					<p>Hello Add Auth0 & Home Page Components in this file.
-					<br>Save and Import any Home page Components from the Components/Home
-					<br>Bye!
-					</p>
+					<p>Hello Add Auth0 & Home Page Components in this file.</p>
+					<p>Save and Import any Home page Components from the Components/Home</p>
+					<p>Reference the Dashboard structure if needed.</p>
+					<p>Delete this text</p>
 				</div>
 			);
 	}
