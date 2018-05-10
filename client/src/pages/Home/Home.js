684efa2728d0bed7b040d0d2df7699d8dc71fb3a
learnesh <32178001+learnesh@users.noreply.github.com>
2018-05-10 18:13:25 -0500

    Updated Home.js
    Home.js updated

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,22 +1,38 @@
-import React, { Component } from "react";
 
+import React, { Component } from 'react';
 
-class Home extends React.Component {
-	
-  
-  
-
-
-	render() {
-		return (
-				<div>
-					<p>Hello Add Auth0 & Home Page Components in this file.</p>
-					<p>Save and Import any Home page Components from the Components/Home</p>
-					<p>Reference the Dashboard structure if needed.</p>
-					<p>Delete this text</p>
-				</div>
-			);
-	}
+class Home extends Component {
+  login() {
+    this.props.auth.login();
+  }
+  render() {
+    const { isAuthenticated } = this.props.auth;
+    return (
+      <div className="container">
+        {
+          isAuthenticated() && (
+              <h4>
+                You are logged in!
+              </h4>
+            )
+        }
+        {
+          !isAuthenticated() && (
+              <h4>
+                You are not logged in! Please{' '}
+                <a
+                  style={{ cursor: 'pointer' }}
+                  onClick={this.login.bind(this)}
+                >
+                  Log In
+                </a>
+                {' '}to continue.
+              </h4>
+            )
+        }
+      </div>
+    );
+  }
 }
 
 export default Home;
