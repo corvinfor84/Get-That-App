a9f0c2fd9a0a76c90c3eaf32bf1bba912120650e
Cornell <corvinforeman@gmail.com>
2018-05-11 10:14:14 -0500

    removed folders

--- client/src/components/SetBudgetBtn/SetBudgetBtn.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./setbudgetbtn.css";
-
- const SetBudgetBtn = (props) => (
- 	<button type="button" className="btn btn-outline-light btn-sm set-budget-btn" onClick={props.addBudget}>{props.btnName}</button>
-			
-);
-
-export default SetBudgetBtn;
--- client/src/components/SetBudgetBtnClear/SetBudgetBtnClear.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./setbudgetbtnclear.css";
-
- const SetBudgetBtnClear = (props) => (
- 	<button type="button" className="btn btn-outline-light btn-sm set-budget-btn-clr">{props.btnName}</button>
-			
-);
-
-export default SetBudgetBtnClear;
