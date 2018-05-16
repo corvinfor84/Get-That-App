5514f63c2a5c5a63b5f340322c1c587b43df2e44
learnesh <32178001+learnesh@users.noreply.github.com>
2018-05-15 20:32:02 -0500

    Update auth0-variables.js

--- client/src/pages/auth0getthat example/client/src/Auth/auth0-variables.js
+++ client/src/pages/auth0getthat example/client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
-  domain: "#", //your key information
-  clientId: "#",
+  domain: "learnesh.auth0.com", //your key information
+  clientId: "BjMWXfCDbSZpK5GqY6E5VeiiSDkUEzKT",
   callbackUrl: "http://localhost:3000/callback"
 };
