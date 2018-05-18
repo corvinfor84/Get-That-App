39c932c7709a0df695cefbb7fabe67d7116313e3
Cornell <corvinforeman@gmail.com>
2018-05-18 02:45:14 -0500

    updated saved links and logout message

--- client/src/components/SavedCard/SavedCard.js
+++ client/src/components/SavedCard/SavedCard.js
@@ -6,7 +6,7 @@ import "./savedcard.css";
  	<div id="saved-menu">
 			<img src={props.savedImage} className="saved-item-image" alt="saved-item-to-calculate"/>
 			<div className="saved-price">{props.savedprice}</div>
-			<p className="saved-title">{props.savedTitle}</p>
+			<p className="saved-title"><a href={props.saveLink} target="_blank">{props.savedTitle}</a></p>
 
 	</div>
 
